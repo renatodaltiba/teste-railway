1811b975827ee8c2be59e50bb255b6b8206d2bb5
Renato Daltiba Schneider <renatodaltiba@gmail.com>
2024-05-15 18:27:41 -0300

    sdas

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import Fastify from 'fastify'
 export const app = Fastify({ logger: true })
 
 app.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  return { hello: 'worsld' }
 })
 
 app.listen({

94857f646d2e0be3731125ef40464ab5faf33c6d
Renato Daltiba Schneider <renatodaltiba@gmail.com>
2024-05-15 18:41:17 -0300

    add port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import Fastify from 'fastify'
 
-
 export const app = Fastify({ logger: true })
 
 app.get('/', async (request, reply) => {
@@ -8,7 +7,7 @@ app.get('/', async (request, reply) => {
 })
 
 app.listen({
-  port: 3000,
+  port: Number(process.env.PORT) || 3000,
 }).then((address) => {
   console.log(`Server listening on ${address}`)
 })
